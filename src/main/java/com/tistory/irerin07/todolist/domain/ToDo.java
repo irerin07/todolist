01140b4426db2b487289c2ebecc462776d200cb0
DESKTOP-QJ641A4\count <counterchord89@gmail.com>
2019-06-12 01:59:48 +0900

    우선 대략적인 파일 먼저 올림. 우선 어떻게 구현하고 설계할지 먼저 생각하고 시작

---
+++ src/main/java/com/tistory/irerin07/todolist/domain/ToDo.java
@@ -0,0 +1,7 @@
+package com.tistory.irerin07.todolist.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class ToDo {
+}
---
+++ src/main/java/com/tistory/irerin07/todolist/repository/ToDoRepository.java
@@ -0,0 +1,4 @@
+package com.tistory.irerin07.todolist.repository;
+
+public class ToDoRepository {
+}
